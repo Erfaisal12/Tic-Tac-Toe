a124d4e3ae8d22ff35c3e4106d8ab9674cb23349
Erfaisal12 <44156067+Erfaisal12@users.noreply.github.com>
2021-07-28 16:12:54 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,61 @@
+board=[" "for i in range(9)]
+
+def print_board():
+    row1="|{}|{}|{}|".format(board[0],board[1],board[2])
+    row2="|{}|{}|{}|".format(board[3],board[4],board[5])
+    row3="|{}|{}|{}|".format(board[6],board[7],board[8]) 
+    print()
+    print(row1)
+    print(row2)
+    print(row3)
+    print()
+
+def player_move(icon):
+    if icon=="X":
+        number=1
+    elif icon=="0":
+        number=2
+    print("Your Turn Player {}".format(number))
+    choice=int(input("Enter numbers 1-9: ").strip())
+    if board[choice-1]==" ":
+        board[choice-1]=icon
+    else:
+        print()
+        print("That Space has taken!")
+
+def is_victory(icon):
+    if (board[0]==icon and board[1]==icon and board[2]==icon)or\
+       (board[3]==icon and board[4]==icon and board[5]==icon)or\
+       (board[6]==icon and board[7]==icon and board[8]==icon)or\
+       (board[0]==icon and board[3]==icon and board[6]==icon)or\
+       (board[1]==icon and board[4]==icon and board[7]==icon)or\
+       (board[2]==icon and board[5]==icon and board[8]==icon)or\
+       (board[0]==icon and board[4]==icon and board[8]==icon)or\
+       (board[2]==icon and board[4]==icon and board[6]==icon):
+        return True
+    else:
+        return False
+def is_draw():
+    if " " not in board:
+      return True
+    else:
+       return False
+    
+while True:
+    print_board()
+    player_move("X")
+    print_board()
+    if is_victory("X"):
+         print("X Wins!.. Congratulations!")
+         break
+    elif is_draw():
+            print("Board is draw!")
+            break
+    
+    player_move("0")
+    if is_victory("0"):
+         print("0 Wins!.. Congratulations!")
+         break
+    elif is_draw():
+            print("Its draw!...")
+            break
